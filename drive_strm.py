0bd0718f112a76a50c1e6b9f761bfbfa686e3196
James <l3uddz@gmail.com>
2018-11-09 19:23:22 +0000

    default chunk size 0.5 MB

--- drive_strm.py
+++ drive_strm.py
@@ -229,7 +229,7 @@ def stream_bridge(request_file):
     return abort(500)
 
 
-def generate_data_from_response(resp, chunk=1000000):
+def generate_data_from_response(resp, chunk=500000):
     for data_chunk in resp.iter_content(chunk_size=chunk):
         yield data_chunk
 
