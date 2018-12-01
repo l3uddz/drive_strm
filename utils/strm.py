f6cbcb528b173243d70628a5c307ac2bbbfeb7e8
desimaniac <desimaniac@users.noreply.github.com>
2018-12-01 16:18:58 -0600

    Add 'original' tag to OG strms when show_transcodes is enabled

--- utils/strm.py
+++ utils/strm.py
@@ -10,21 +10,27 @@ transcode_versions = ['1080', '720', '480', '360']
 
 
 def write_strms(cfg, file_id, file_paths):
+
     strm_url = f"{cfg.strm.access_url.rstrip('/')}/strm/{file_id}"
     root_path = cfg.strm.root_path
+
     for file_path in file_paths:
 
-        # get basename
+        # set file_path to basename
         file_path = os.path.splitext(file_path)[0]
 
-        # remove quality tags
-        file_path = re.sub(r'\.?\s?(2160|1080|720|480|360)p', "", file_path, flags=re.IGNORECASE)
-
         # set versions to write
-        files_to_write = {'OG': os.path.join(root_path, f'{file_path}.strm')}
         if cfg.strm.show_transcodes:
+
+            # remove quality tags
+            file_path = re.sub(r'\.?\s?(2160|1080|720|480|360)p', "", file_path, flags=re.IGNORECASE)
+
+            files_to_write = {'OG': os.path.join(root_path, f'{file_path} - original.strm')}
+
             for version in transcode_versions:
                 files_to_write[version] = os.path.join(root_path, f'{file_path} - {version}.strm')
+        else:
+            files_to_write = {'OG': os.path.join(root_path, f'{file_path}.strm')}
 
         # write strms
         for strm_version, new_file_path in files_to_write.items():
@@ -35,21 +41,27 @@ def write_strms(cfg, file_id, file_paths):
 
 
 def remove_strms(cfg, file_paths):
+
     root_path = cfg.strm.root_path
     sorted_paths = path.sort_path_list(file_paths)
+
     for file_path in sorted_paths:
 
         # get basename
         file_path = os.path.splitext(file_path)[0]
 
-        # remove quality tags
-        file_path = re.sub(r'\.?\s?(2160|1080|720|480|360)p', "", file_path, flags=re.IGNORECASE)
-
         # set versions to remove
-        files_to_remove = {'OG': os.path.join(root_path, f'{file_path}.strm')}
         if cfg.strm.show_transcodes:
+
+            # remove quality tags
+            file_path = re.sub(r'\.?\s?(2160|1080|720|480|360)p', "", file_path, flags=re.IGNORECASE)
+
+            files_to_remove = {'OG': os.path.join(root_path, f'{file_path} - original.strm')}
+
             for version in transcode_versions:
                 files_to_remove[version] = os.path.join(root_path, f'{file_path} - {version}.strm')
+        else:
+            files_to_remove = {'OG': os.path.join(root_path, f'{file_path}.strm')}
 
         for strm_version, new_file_path in files_to_remove.items():
             log.debug(f"Removing STRM: {new_file_path}")
